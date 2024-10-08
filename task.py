31b713e32f62014e5bc50cdc7e86c75d6b86abf5
unknown <swietonb@icloud.com>
2024-10-08 21:35:09 +0200

    Update 4.1

--- task.py
+++ task.py
@@ -4,7 +4,7 @@ class Task:
         self.priority = priority
         self.category = category
         self.is_deleted = False
-        self.is_completed = False
+        self.is_completed = False #Kom
 
     def toggle_completed(self):
         """Przełącz status ukończenia zadania"""
